0896588691fdcbd10862b76bd12c6b99ad906f45
mahesh_01 <cmahesh91@gmail.com>
2019-09-09 12:11:48 +0530

    first commit

---
+++ JAVAProgrammes/src/com/Inheritance/AB.java
@@ -0,0 +1,18 @@
+package com.Inheritance;
+class cd{
+	public void add() {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+	}
+	
+}
+public class AB extends cd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AB obj =new AB();
+obj.add();
+	}
+
+}
---
+++ JAVAProgrammes/src/com/Programmes/Pattern5.java
@@ -0,0 +1,18 @@
+package com.Programmes;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int a=1; a<=4; a++)
+		{
+			for(int b=1; b<=a;b++)
+			{
+				System.out.print(a);
+			}
+			System.out.println(" ");
+			
+		}
+	}
+
+}
